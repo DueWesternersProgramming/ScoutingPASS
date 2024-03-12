19c8efd323b16665e30a69e7ad94688f5324e551
DueWesternersProgramming <72952036+DueWesternersProgramming@users.noreply.github.com>
2024-03-12 19:42:59 -0400

    Second Season Event

--- 2024/crescendo_config.js
+++ 2024/crescendo_config.js
@@ -15,7 +15,7 @@ var config_data = `
     { "name": "Event",
       "code": "e",
       "type": "event",
-      "defaultValue": "2024scand",
+      "defaultValue": "2024sccha",
       "required": "true"
     },
     { "name": "Match Level",
