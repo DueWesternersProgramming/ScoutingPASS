baf88785bcc1f6679d6c62ed29a96c9b6eb6c902
DueWesternersProgramming <frcprogrammers@duewestrobotics.org>
2024-03-26 19:11:22 -0400

    Updated Event

--- 2024/crescendo_config.js
+++ 2024/crescendo_config.js
@@ -15,7 +15,7 @@ var config_data = `
     { "name": "Event",
       "code": "e",
       "type": "event",
-      "defaultValue": "2024sccha",
+      "defaultValue": "2024gacmp",
       "required": "true"
     },
     { "name": "Match Level",
