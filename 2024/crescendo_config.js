851ce3a445d89dd1c93aae1f6f681cfea5d90556
Joel Logan <76826688+JoelLogan@users.noreply.github.com>
2024-04-11 16:43:34 -0400

    Update crescendo_config.js

--- 2024/crescendo_config.js
+++ 2024/crescendo_config.js
@@ -15,7 +15,7 @@ var config_data = `
     { "name": "Event",
       "code": "e",
       "type": "event",
-      "defaultValue": "2024cmptx",
+      "defaultValue": "2024curie",
       "required": "true"
     },
     { "name": "Match Level",
